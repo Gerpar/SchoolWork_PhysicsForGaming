7363ad10751653a0176e52e81377bd9feb59e6c3
Gerpar <Gerpar.gerps@gmail.com>
2020-03-31 22:42:55 -0400

    Finished first half of assignment 4

---
+++ Project 4/Gparis1_Project 4 - Energy/Project 4 - Energy/Form1.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace Project_4___Energy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBulletV = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBulletWtA = new System.Windows.Forms.TextBox();
+            this.lblBulletHt = new System.Windows.Forms.Label();
+            this.lblBulletV = new System.Windows.Forms.Label();
+            this.lblBulletWt = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBallHt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBallWtA = new System.Windows.Forms.TextBox();
+            this.lblBallV = new System.Windows.Forms.Label();
+            this.lblBallHt = new System.Windows.Forms.Label();
+            this.lblBallWt = new System.Windows.Forms.Label();
+            this.lblBullet = new System.Windows.Forms.Label();
+            this.lblDrop = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnNudge = new System.Windows.Forms.Button();
+            this.btnFire = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBallWtB = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtBulletWtB = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(875, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 42;
+            this.label5.Text = "m/s";
+            // 
+            // txtBulletV
+            // 
+            this.txtBulletV.Location = new System.Drawing.Point(812, 195);
+            this.txtBulletV.Name = "txtBulletV";
+            this.txtBulletV.Size = new System.Drawing.Size(57, 20);
+            this.txtBulletV.TabIndex = 7;
+            this.txtBulletV.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(875, 137);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 40;
+            this.label6.Text = "ounces";
+            // 
+            // txtBulletWtA
+            // 
+            this.txtBulletWtA.Location = new System.Drawing.Point(812, 130);
+            this.txtBulletWtA.Name = "txtBulletWtA";
+            this.txtBulletWtA.Size = new System.Drawing.Size(57, 20);
+            this.txtBulletWtA.TabIndex = 5;
+            this.txtBulletWtA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblBulletHt
+            // 
+            this.lblBulletHt.AutoSize = true;
+            this.lblBulletHt.Location = new System.Drawing.Point(715, 307);
+            this.lblBulletHt.Name = "lblBulletHt";
+            this.lblBulletHt.Size = new System.Drawing.Size(128, 13);
+            this.lblBulletHt.TabIndex = 38;
+            this.lblBulletHt.Text = "Maximum height reached:";
+            this.lblBulletHt.Visible = false;
+            // 
+            // lblBulletV
+            // 
+            this.lblBulletV.AutoSize = true;
+            this.lblBulletV.Location = new System.Drawing.Point(715, 202);
+            this.lblBulletV.Name = "lblBulletV";
+            this.lblBulletV.Size = new System.Drawing.Size(73, 13);
+            this.lblBulletV.TabIndex = 37;
+            this.lblBulletV.Text = "Initial velocity:";
+            // 
+            // lblBulletWt
+            // 
+            this.lblBulletWt.AutoSize = true;
+            this.lblBulletWt.Location = new System.Drawing.Point(715, 137);
+            this.lblBulletWt.Name = "lblBulletWt";
+            this.lblBulletWt.Size = new System.Drawing.Size(94, 13);
+            this.lblBulletWt.TabIndex = 36;
+            this.lblBulletWt.Text = "Weight of bullet A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(421, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "m";
+            // 
+            // txtBallHt
+            // 
+            this.txtBallHt.Location = new System.Drawing.Point(358, 188);
+            this.txtBallHt.Name = "txtBallHt";
+            this.txtBallHt.Size = new System.Drawing.Size(57, 20);
+            this.txtBallHt.TabIndex = 3;
+            this.txtBallHt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(421, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "pounds";
+            // 
+            // txtBallWtA
+            // 
+            this.txtBallWtA.Location = new System.Drawing.Point(358, 130);
+            this.txtBallWtA.Name = "txtBallWtA";
+            this.txtBallWtA.Size = new System.Drawing.Size(57, 20);
+            this.txtBallWtA.TabIndex = 1;
+            this.txtBallWtA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblBallV
+            // 
+            this.lblBallV.AutoSize = true;
+            this.lblBallV.Location = new System.Drawing.Point(271, 300);
+            this.lblBallV.Name = "lblBallV";
+            this.lblBallV.Size = new System.Drawing.Size(157, 13);
+            this.lblBallV.TabIndex = 30;
+            this.lblBallV.Text = "Velocity when it hits the ground:";
+            this.lblBallV.Visible = false;
+            // 
+            // lblBallHt
+            // 
+            this.lblBallHt.AutoSize = true;
+            this.lblBallHt.Location = new System.Drawing.Point(271, 195);
+            this.lblBallHt.Name = "lblBallHt";
+            this.lblBallHt.Size = new System.Drawing.Size(69, 13);
+            this.lblBallHt.TabIndex = 29;
+            this.lblBallHt.Text = "Height of fall:";
+            // 
+            // lblBallWt
+            // 
+            this.lblBallWt.AutoSize = true;
+            this.lblBallWt.Location = new System.Drawing.Point(271, 137);
+            this.lblBallWt.Name = "lblBallWt";
+            this.lblBallWt.Size = new System.Drawing.Size(85, 13);
+            this.lblBallWt.TabIndex = 28;
+            this.lblBallWt.Text = "Weight of ball A:";
+            // 
+            // lblBullet
+            // 
+            this.lblBullet.AutoSize = true;
+            this.lblBullet.Location = new System.Drawing.Point(525, 45);
+            this.lblBullet.Name = "lblBullet";
+            this.lblBullet.Size = new System.Drawing.Size(351, 39);
+            this.lblBullet.TabIndex = 27;
+            this.lblBullet.Text = resources.GetString("lblBullet.Text");
+            // 
+            // lblDrop
+            // 
+            this.lblDrop.AutoSize = true;
+            this.lblDrop.Location = new System.Drawing.Point(79, 45);
+            this.lblDrop.Name = "lblDrop";
+            this.lblDrop.Size = new System.Drawing.Size(351, 39);
+            this.lblDrop.TabIndex = 26;
+            this.lblDrop.Text = "Using the Principle of Conservation of Mechanical Energy to demonstrate\r\nthat the" +
+    " velocity of an object in free fall depends on the height of the fall\r\nbut does " +
+    "not depend on the weight of the object.";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(545, 94);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(182, 336);
+            this.pictureBox2.TabIndex = 25;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(79, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(191, 336);
+            this.pictureBox1.TabIndex = 24;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnNudge
+            // 
+            this.btnNudge.Location = new System.Drawing.Point(300, 254);
+            this.btnNudge.Name = "btnNudge";
+            this.btnNudge.Size = new System.Drawing.Size(128, 23);
+            this.btnNudge.TabIndex = 4;
+            this.btnNudge.Text = "Nudge Ball Over Edge";
+            this.btnNudge.UseVisualStyleBackColor = true;
+            this.btnNudge.Click += new System.EventHandler(this.btnNudge_Click);
+            // 
+            // btnFire
+            // 
+            this.btnFire.Location = new System.Drawing.Point(757, 254);
+            this.btnFire.Name = "btnFire";
+            this.btnFire.Size = new System.Drawing.Size(75, 23);
+            this.btnFire.TabIndex = 8;
+            this.btnFire.Text = "Fire Gun";
+            this.btnFire.UseVisualStyleBackColor = true;
+            this.btnFire.Click += new System.EventHandler(this.btnFire_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(523, 436);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 45;
+            this.btnClear.Text = "Clear All";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(421, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 48;
+            this.label3.Text = "pounds";
+            // 
+            // txtBallWtB
+            // 
+            this.txtBallWtB.Location = new System.Drawing.Point(358, 159);
+            this.txtBallWtB.Name = "txtBallWtB";
+            this.txtBallWtB.Size = new System.Drawing.Size(57, 20);
+            this.txtBallWtB.TabIndex = 2;
+            this.txtBallWtB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(271, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "Weight of ball B:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(875, 166);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 51;
+            this.label7.Text = "ounces";
+            // 
+            // txtBulletWtB
+            // 
+            this.txtBulletWtB.Location = new System.Drawing.Point(812, 159);
+            this.txtBulletWtB.Name = "txtBulletWtB";
+            this.txtBulletWtB.Size = new System.Drawing.Size(57, 20);
+            this.txtBulletWtB.TabIndex = 6;
+            this.txtBulletWtB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(715, 166);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 13);
+            this.label8.TabIndex = 49;
+            this.label8.Text = "Weight of bullet B:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 465);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtBulletWtB);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBallWtB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnFire);
+            this.Controls.Add(this.btnNudge);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBulletV);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtBulletWtA);
+            this.Controls.Add(this.lblBulletHt);
+            this.Controls.Add(this.lblBulletV);
+            this.Controls.Add(this.lblBulletWt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBallHt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBallWtA);
+            this.Controls.Add(this.lblBallV);
+            this.Controls.Add(this.lblBallHt);
+            this.Controls.Add(this.lblBallWt);
+            this.Controls.Add(this.lblBullet);
+            this.Controls.Add(this.lblDrop);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBulletV;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtBulletWtA;
+        private System.Windows.Forms.Label lblBulletHt;
+        private System.Windows.Forms.Label lblBulletV;
+        private System.Windows.Forms.Label lblBulletWt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBallHt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBallWtA;
+        private System.Windows.Forms.Label lblBallV;
+        private System.Windows.Forms.Label lblBallHt;
+        private System.Windows.Forms.Label lblBallWt;
+        private System.Windows.Forms.Label lblBullet;
+        private System.Windows.Forms.Label lblDrop;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnNudge;
+        private System.Windows.Forms.Button btnFire;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBallWtB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtBulletWtB;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Project 4/Gparis1_Project 4 - Energy/Project 4 - Energy/Form1.cs
@@ -0,0 +1,106 @@
+﻿///-----------------------------------------------------------------------------------------------------------------///
+//|	Name: Gerad Paris											                                                    |//
+//|	Due Date: 04/01/2020										                                                    |//
+//|	Instructor: Steven MacNaughton  							                                                    |//
+//| Purpose: 
+///-----------------------------------------------------------------------------------------------------------------///
+///Progress:
+///
+/// 03/31/2020
+///+ Created final Velocity function
+///+ Created Weight to mass function
+///+ Created kinetic energy function
+///+ Finished implementing btnNudge_Click
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_4___Energy
+{
+    public partial class Form1 : Form
+    {
+        const double ERROR_NUM = -999;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double ConvertToDouble(String text, String errorMsg)
+        {
+            try
+            {
+                double local = Convert.ToDouble(text);
+                return local;
+            }
+            catch 
+			{
+                MessageBox.Show("Invalid entry for " + errorMsg);
+                return ERROR_NUM;
+            }
+        }
+
+        private void btnNudge_Click(object sender, EventArgs e)
+        {
+            double ballAWeight, ballBWeight, ballHeight;
+            // If all entries are valid
+            if ((ballAWeight = ConvertToDouble(txtBallWtA.Text, "Ball A.")) != ERROR_NUM && (ballBWeight = ConvertToDouble(txtBallWtB.Text, "Ball B.")) != ERROR_NUM && (ballHeight = ConvertToDouble(txtBallHt.Text, "Ball height.")) != ERROR_NUM)
+            {
+                double finalVel = -CalculateFinalVelocity(0, -9.8, -ballHeight); // Calculate final velocity
+                double ballAKE = CalculateKineticEnergy(WeightToMass(ballAWeight), finalVel);   // Calculate ball A Kinetic Energy before hitting ground
+                double ballBKE = CalculateKineticEnergy(WeightToMass(ballBWeight), finalVel);   // Calculate ball B Kinetic Energy before hitting ground
+
+                // Output text
+                lblBallV.Text = "Ball A Kinetic energy when hitting the ground: " + Convert.ToString(Math.Round(ballAKE,2)) + "J\n";
+                lblBallV.Text += "Final velocity of ball A: " + Convert.ToString(Math.Round(finalVel, 2)) + "m/s\n\n";
+
+                lblBallV.Text += "Ball B Kinetic energy when hitting the ground: " + Convert.ToString(Math.Round(ballBKE, 2)) + "J\n";
+                lblBallV.Text += "Final Velocity of ball B: " + Convert.ToString(Math.Round(finalVel, 2)) + "m/s\n\n"; 
+                lblBallV.Visible = true;    // Show output text
+                btnNudge.Visible = false;   // Hide nudge button
+            }
+        }
+
+        private void btnFire_Click(object sender, EventArgs e)
+        {
+            // Your Code Here
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Your Code Here
+        }
+
+        // CALCULATION FUNCTIONS
+        //--------------------
+
+        // Calculates final velocity based on passed parameters
+        private double CalculateFinalVelocity(double initialVelocity, double acceleration, double distanceDelta)
+        {
+            double result = Math.Sqrt(Math.Pow(initialVelocity, 2) + (2 * acceleration * distanceDelta));
+            return result;
+        }
+
+        // Converts lbs into mass in KG
+        private double WeightToMass(double weight)
+        {
+            double result = weight / 0.2248;    // Convert lbs to N
+            result = result / 9.8;              // Convert N to KG
+            return result;
+        }
+
+        // Calculates the Kinetic energy of the object using it's mass and velocity
+        private double CalculateKineticEnergy(double mass, double velocity)
+        {
+            double result =  0.5 * (mass * Math.Pow(velocity, 2));  // Calculate KEf
+            return result;
+        }
+    }
+}
